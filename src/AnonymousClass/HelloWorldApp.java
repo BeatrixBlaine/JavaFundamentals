4bd929791b2903fb65deaa4d5fe0a1c358f1a261
Icad <beatrixblaine@gmail.com>
2026-04-27 11:31:25 +0700

    Anonymous class

---
+++ src/AnonymousClass/HelloWorldApp.java
@@ -0,0 +1,25 @@
+package AnonymousClass;
+
+public class HelloWorldApp {
+
+    public static void main(String[] args) {
+
+        // Anonymous Class, creating instance from HelloWorld interface
+        // normally interface should be implemented to other classes, then use it in the main app
+        HelloWorld obj = new HelloWorld()/* <-- this is the anonymous class */ {
+            @Override
+            public void sayHello() {
+                System.out.println("Hello World!");
+            }
+
+            @Override
+            public void sayHello(String name) {
+                System.out.println("Hello " + name);
+            }
+        };
+
+        obj.sayHello();
+        obj.sayHello("Icad");
+
+    }
+}
