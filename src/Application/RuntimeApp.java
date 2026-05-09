5389d13ba1604dd1947423624b21193234d09416
Icad <beatrixblaine@gmail.com>
2026-05-09 15:46:44 +0700

    exec in Runtime

--- src/Application/RuntimeApp.java
+++ src/Application/RuntimeApp.java
@@ -1,12 +1,30 @@
 package Application;
 
+import java.io.IOException;
+
 public class RuntimeApp {
     public static void main(String[] args) {
 
-        Runtime runtime = Runtime.getRuntime();
+        try {
+            Runtime runtime = Runtime.getRuntime();
 
-        System.out.println(runtime.availableProcessors());
-        System.out.println(runtime.freeMemory());
+            String[] cmd = {"notepad.exe"};
+            long bytes = runtime.totalMemory();
+            double GB = bytes / (1024.0 * 1024 * 1024);
+
+            System.out.println(runtime.availableProcessors());
+            System.out.println(runtime.totalMemory() + " bytes");
+            System.out.println(runtime.freeMemory());
+            System.out.println(GB + " GB");
+
+            // execute notepad
+            runtime.exec(cmd);
+
+
+        } catch (IOException exception) {
+            System.out.println("File tidak bisa di load!");
+        }
+
 
     }
 }
